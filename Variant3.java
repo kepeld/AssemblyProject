df1bbe66e087e88fca2fbe5b794a180785840486
kepeld <a.kozin@ukma.edu.ua>
2024-03-14 00:27:19 +0200

    Added code to initialize Scanner for reading from standard input and two HashMaps (sumMap and countMap) for storing sum and count values for each key.

--- Variant3.java
+++ Variant3.java
@@ -1,3 +1,7 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Variant3 {
     static class Pair<A, B> {
         public final A key;
@@ -8,4 +12,8 @@ public class Variant3 {
             this.value = value;
         }
     }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Map<String, Integer> sumMap = new HashMap<>();
+        Map<String, Integer> countMap = new HashMap<>();
 }
