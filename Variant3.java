1c8f4e55c49dc9486d067144010fb5eee7a872e0
kepeld <a.kozin@ukma.edu.ua>
2024-03-14 00:24:36 +0200

    Added the Pair class for storing key-value pairs.

---
+++ Variant3.java
@@ -0,0 +1,11 @@
+public class Variant3 {
+    static class Pair<A, B> {
+        public final A key;
+        public final B value;
+
+        public Pair(A key, B value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
